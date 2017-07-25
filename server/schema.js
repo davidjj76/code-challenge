1580f13e241f469f4d00b595bc3ffdda7bd4fd41
David Jiménez <djimenez@ets.es>
2017-07-25 17:08:17 +0200

    Add insert, update and delete articles mutations

--- server/schema.js
+++ server/schema.js
@@ -45,7 +45,6 @@ const Query = new GraphQLObjectType({
       type: articleType,
       args: {
         id: {
-          name: 'id',
           type: new GraphQLNonNull(GraphQLString),
         },
       },
@@ -63,8 +62,89 @@ const Query = new GraphQLObjectType({
   }),
 });
 
+const Mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  description: 'This is a root mutation',
+  fields: () => ({
+    createArticle: {
+      description: 'creates a new article',
+      type: articleType,
+      args: {
+        author: {
+          type: new GraphQLNonNull(GraphQLString),
+        },
+        content: {
+          type: new GraphQLNonNull(GraphQLString),
+        },
+        excerpt: {
+          type: new GraphQLNonNull(GraphQLString),
+        },
+        published: {
+          type: GraphQLBoolean,
+          defaultValue: true,
+        },
+        tags: {
+          type: new GraphQLList(GraphQLString),
+          defaultValue: [],
+        },
+        title: {
+          type: new GraphQLNonNull(GraphQLString),
+        },
+      },
+      resolve(root, { author, content, excerpt, published, tags, title }) {
+        return db.Article.create({ author, content, excerpt, published, tags, title });
+      },
+    },
+    updateArticle: {
+      description: 'modify an existing article',
+      type: articleType,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLString),
+        },
+        author: {
+          type: GraphQLString,
+        },
+        content: {
+          type: GraphQLString,
+        },
+        excerpt: {
+          type: GraphQLString,
+        },
+        published: {
+          type: GraphQLBoolean,
+          defaultValue: true,
+        },
+        tags: {
+          type: new GraphQLList(GraphQLString),
+          defaultValue: [],
+        },
+        title: {
+          type: GraphQLString,
+        },
+      },
+      resolve(root, { id, ...article }) {
+        return db.Article.findByIdAndUpdate(id, article);
+      },
+    },
+    deleteArticle: {
+      description: 'deletes an article by id',
+      type: articleType,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLString),
+        },
+      },
+      resolve(root, { id }) {
+        return db.Article.findByIdAndRemove(id);
+      },
+    },
+  }),
+});
+
 const Schema = new GraphQLSchema({
   query: Query,
+  mutation: Mutation,
 });
 
 export default Schema;
