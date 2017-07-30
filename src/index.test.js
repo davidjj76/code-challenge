9ec7e1e23e2306a964d9cab882e8265e8b8efb40
davidjj76 <davidjj76@gmail.com>
2017-07-30 16:24:28 +0200

    Fix index.test.js

--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import App from './components/App';
+
+const middleware = [thunk];
+const mockStore = configureStore(middleware);
+const initialState = {
+  isFetching: false,
+  err: null,
+  data: {
+    articles: [],
+    article: {
+      author: '',
+      content: '',
+      published: false,
+      tags: [],
+      title: '',
+    },
+  },
+};
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  render(
+    <Provider store={mockStore(initialState)}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>, div);
 });
